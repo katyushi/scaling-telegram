29a9a59f4102893169251b9ee9db70c248937a87
hugo lazzari <hugo.lazzari@wapstore.com.br>
2022-06-07 19:41:54 -0300

    adicionado vue-router e parte do componente de pessoas

---
+++ src/components/peopleSearcher.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'peopleSearcher',
+
+  data() {
+    return {
+      peopleInfo: []
+    }
+  },
+
+  methods: {
+    searchPeople() {
+      const baseURL = "https://swapi.dev/api/people/";
+      this.$http.get(baseURL).then((response) => {
+        this.peopleInfo = response.data;
+      })
+    }
+  },
+
+  created() {
+    this.searchPeople()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
